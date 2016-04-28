d7f0b14c0d5072add8dc41a5726121b6537de3e7
Kartik Null Cating-Subramanian <ksubramanian@chef.io>
2016-04-28 11:41:32 -0400

    Generated cookbook for mingw

---
+++ README.md
@@ -0,0 +1,4 @@
+# mingw
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,14 @@
+name 'mingw'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license 'apache2'
+description 'Installs a mingw/msys based toolchain on windows'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.1.0'
+
+supports 'windows'
+
+depends 'seven_zip'
+
+source_url 'https://github.com/chef-cookbooks/mingw'
+issues_url 'https://github.com/chef-cookbooks/mingw/issues'
