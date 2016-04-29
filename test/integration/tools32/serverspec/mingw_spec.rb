6e74c5014deec84b7eea318287a5c9f82c45e651
Kartik Null Cating-Subramanian <ksubramanian@chef.io>
2016-04-29 14:41:26 -0400

    Simplify the installation of base files

--- test/integration/tools32/serverspec/mingw_spec.rb
+++ test/integration/tools32/serverspec/mingw_spec.rb
@@ -2,14 +2,24 @@ require 'spec_helper'
 
 set :path, "C:\\mingw32\\bin;C:\\mingw32\\msys\\1.0\\bin;#{ENV['PATH']}"
 
-describe command("gcc -v") do
+describe command("gcc --version") do
   its(:exit_status) { should eq 0 }
-  its(:stdout) { should contain 'Target: mingw32' }
-  its(:stdout) { should contain 'gcc version 5.1.0 (tdm-1)' }
+  its(:stdout) { should include 'gcc.exe (tdm-1) 5.1.0' }
+end
+
+describe command("gcc -dumpmachine") do
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should eq "mingw32\n" }
+end
+
+describe command("as --version") do
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should include "2.25.1" }
+  its(:stdout) { should include "mingw32" }
 end
 
 describe command("make --version") do
   its(:exit_status) { should eq 0 }
-  its(:stdout) { should contain 'GNU Make 3.81' }
+  its(:stdout) { should include 'GNU Make 3.81' }
 end
 
