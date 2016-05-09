f6807222ce45fcb2e5c2a5a6cb881059de994171
Tim Smith <tsmith84@gmail.com>
2016-05-09 09:14:19 -0700

    Use the full license name

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'mingw'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license 'apache2'
+license 'Apache 2.0'
 description 'Installs a mingw/msys based toolchain on windows'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
