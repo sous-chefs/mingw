b31e688a55fed41cb641da627e1598889d588118
Tim Smith <tsmith84@gmail.com>
2016-05-11 11:46:15 -0700

    Release 1.0.0
    Signed-off-by: Tim Smith <tsmith84@gmail.com>

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs a mingw/msys based toolchain on windows'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.1'
+version '1.0.0'
 
 supports 'windows'
 
