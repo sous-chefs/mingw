7e232f92a3ac1b26d345444492c4e456a3b97dcf
Kartik Null Cating-Subramanian <ksubramanian@chef.io>
2016-06-08 13:54:43 -0400

    Updating to fix the issue where bash does not inherit the cwd correctly

--- files/default/bash.bat
+++ files/default/bash.bat
@@ -1,3 +1,6 @@
-@set ORIGINAL_PWD=%CD%
-@set HOME=%~dp0..\home\%USERNAME%
+@set HOME=/home/%USERNAME%
+@rem See /etc/profile - it should invoke post-install step 05-home-dir.post
+@rem which uses this environment variable to change directories.
+@set CHERE_INVOKING=1
 %~dp0..\usr\bin\bash.exe -l %*
+@rem >> %~dp0logoutput.txt
