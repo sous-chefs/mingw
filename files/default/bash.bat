8c8f829b20978ff40aa94c07f944b2e9a944c6e8
Kartik Null Cating-Subramanian <ksubramanian@chef.io>
2016-07-25 21:54:56 -0400

    Un-mess up msys2's helpful PATH protection

--- files/default/bash.bat
+++ files/default/bash.bat
@@ -10,6 +10,9 @@ IF "%MSYSTEM%"=="" (
   exit
 )
 
+rem Damn you msys2 - stop screwing with my envrionment variables...
+set MSYS2_PATH_TYPE=inherit
+
 rem See /etc/profile - it should invoke post-install step 05-home-dir.post
 rem which uses this environment variable to change directories.
 set CHERE_INVOKING=1
