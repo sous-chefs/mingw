cfef1f6c592645b1c7124a9920d74c087d4c104e
Tim Smith <tsmith84@gmail.com>
2019-10-07 16:33:21 -0700

    Resolve cookstyle 5.8 warnings
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Installs a mingw/msys based toolchain on windows'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.1.0'
 
 supports 'windows'
@@ -12,4 +11,4 @@ depends 'seven_zip'
 
 source_url 'https://github.com/chef-cookbooks/mingw'
 issues_url 'https://github.com/chef-cookbooks/mingw/issues'
-chef_version '>= 12.5' if respond_to?(:chef_version)
+chef_version '>= 12.5'
